f5c6b1b3abd850e353e014c284768ae1f0fd2e98
Christian Gehring <gehrinch@ethz.ch>
2013-11-22 10:09:52 +0100

    Added copyright to source files

--- rm_linear_algebra/include/rm/linear_algebra/LinearAlgebra.hpp
+++ rm_linear_algebra/include/rm/linear_algebra/LinearAlgebra.hpp
@@ -1,10 +1,30 @@
 /*
- * LinearAlgebra.hpp
+ * Copyright (c) 2013, Christian Gehring, Hannes Sommer, Paul Furgale, Péter Fankhauser
+ * All rights reserved.
  *
- *  Created on: October 16, 2012
- *      Author: gech, Peter Fankhauser
- */
-
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the <organization> nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+*/
 #ifndef RM_LINEARALGEBRA_HPP_
 #define RMLINEARALGEBRA_HPP_
 
@@ -13,18 +33,25 @@
 namespace rm {
 namespace linear_algebra {
 
-/*! Gets a skew-symmetric matrix from a vector
- * @param vec vector
- * @return skew matrix
+/*!
+ * \brief Gets a skew-symmetric matrix from a (column) vector
+ * \param   vec 3x1-matrix (column vector)
+ * \return skew   3x3-matrix
  */
-Eigen::Matrix3d getSkewMatrixFromVector(const Eigen::Vector3d& vec);
+inline Eigen::Matrix3d getSkewMatrixFromVector(const Eigen::Vector3d& vec) {
+  Eigen::Matrix3d mat;
+  mat << 0, -vec(2), vec(1), vec(2), 0, -vec(0), -vec(1), vec(0), 0;
+  return mat;
+}
 
-/*! Computes the Moore–Penrose pseudoinverse
+
+/*!
+ * \brief Computes the Moore–Penrose pseudoinverse
  * info: http://eigen.tuxfamily.org/bz/show_bug.cgi?id=257
- * @param a: Matrix to invert
- * @param result: Result is written here
- * @param epsilon: Numerical precision (for example 1e-6)
- * @return true if successful
+ * \param a: Matrix to invert
+ * \param result: Result is written here
+ * \param epsilon: Numerical precision (for example 1e-6)
+ * \return true if successful
  */
 template<typename _Matrix_Type_>
 bool pseudoInverse(const _Matrix_Type_ &a, _Matrix_Type_ &result, double epsilon = std::numeric_limits<typename _Matrix_Type_::Scalar>::epsilon())
