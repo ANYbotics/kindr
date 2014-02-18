889ae677e144a0c7bb64ff2afcbc560fb5b76ae2
Christian Gehring <gehrinch@ethz.ch>
2014-02-18 21:23:59 +0100

    Changed old namespace rm to kindr

--- include/kindr/common/source_file_pos.hpp
+++ include/kindr/common/source_file_pos.hpp
@@ -2,16 +2,16 @@
  * Copyright (c) 2013, Christian Gehring, Hannes Sommer, Paul Furgale, Remo Diethelm
  * All rights reserved.
  *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
+ * Redistribution and use in source and binary fokindrs, with or without
+ * modification, are pekindritted provided that the following conditions are met:
  *     * Redistributions of source code must retain the above copyright
  *       notice, this list of conditions and the following disclaimer.
- *     * Redistributions in binary form must reproduce the above copyright
+ *     * Redistributions in binary fokindr must reproduce the above copyright
  *       notice, this list of conditions and the following disclaimer in the
  *       documentation and/or other materials provided with the distribution.
  *     * Neither the name of the Autonomous Systems Lab, ETH Zurich nor the
  *       names of its contributors may be used to endorse or promote products
- *       derived from this software without specific prior written permission.
+ *       derived from this software without specific prior written pekindrission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
@@ -59,7 +59,7 @@ namespace kindr {
 
   };
 
-}// namespace rm
+}// namespace kindr
 
 inline std::ostream & operator<<(std::ostream & out, const kindr::source_file_pos & sfp)
 {
@@ -68,7 +68,7 @@ inline std::ostream & operator<<(std::ostream & out, const kindr::source_file_po
 }
 
 
-#define KINDR_SOURCE_FILE_POS rm::source_file_pos(__FUNCTION__,__FILE__,__LINE__)
+#define KINDR_SOURCE_FILE_POS kindr::source_file_pos(__FUNCTION__,__FILE__,__LINE__)
 
 #endif
 
