090b5d6560b2cc4d0bf42423e0be6aa9e905a6dc
Christian Gehring <gehrinch@ethz.ch>
2014-03-04 14:44:57 +0100

    Removed FindKindr.cmake

--- cmake/FindKindr.cmake
+++
@@ -1,9 +0,0 @@
-# This file was automatically generated during the installation of the Kindr library
-# and can be used through cmake to find the corresponding header files. A copy of this
-# file was created in /usr/share/cmake-2.8/Modules (depending on the CMAKE_ROOT variable).
-
-set(Kindr_INCLUDE_DIRS
-/usr/local/include/Kindr/include
-)
-set(Kindr_FOUND TRUE)
-message("-- Kindr found (include: /usr/local/include/Kindr/include)")
