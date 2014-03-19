6ee8b188321a39ae8229064d8dc8da304c271b6b
Remo Diethelm <remod@student.ethz.ch>
2014-03-19 16:21:31 +0100

    added unit tests for Quaternion::vector() and Quaternion::Quaternion(Vector4)

--- test/quaternions/QuaternionEigenTest.cpp
+++ test/quaternions/QuaternionEigenTest.cpp
@@ -306,6 +306,28 @@ TYPED_TEST (QuaternionsSingleTest, testQuaternionSingleImplementationCast) {
   ASSERT_EQ(testEigenQuat.z(),this->eigenQuat1.z());
 }
 
+// Testing vector() and constructor from vector
+TYPED_TEST (QuaternionsSingleTest, testQuaternionSingleVectorAndVectorConstructor) {
+  typedef typename TestFixture::Quaternion Quaternion;
+  typedef typename TestFixture::QuaternionScalar QuaternionScalar;
+  typedef Eigen::Matrix<QuaternionScalar, 4, 1> Vector4;
+  Quaternion quat = this->quat1;
+
+  // vector()
+  Vector4 vector = quat.vector();
+  ASSERT_NEAR(quat.w(),vector(0), 1e-3);
+  ASSERT_NEAR(quat.x(),vector(1), 1e-3);
+  ASSERT_NEAR(quat.y(),vector(2), 1e-3);
+  ASSERT_NEAR(quat.z(),vector(3), 1e-3);
+
+  // constructor from vector
+  Quaternion quatFromVector(vector);
+  ASSERT_NEAR(quat.w(),quatFromVector.w(), 1e-3);
+  ASSERT_NEAR(quat.x(),quatFromVector.x(), 1e-3);
+  ASSERT_NEAR(quat.y(),quatFromVector.y(), 1e-3);
+  ASSERT_NEAR(quat.z(),quatFromVector.z(), 1e-3);
+}
+
 // Testing of special matrices
 TYPED_TEST (QuaternionsSingleTest, testQuaternionSingleSpecialMatrices) {
   typedef typename TestFixture::Quaternion Quaternion;
@@ -577,6 +599,28 @@ TYPED_TEST (UnitQuaternionsSingleTest, testUnitQuaternionSingleSpecialMatrices) 
   ASSERT_NEAR(concatenation3.x(),concatenation4.x(), 1e-3);
   ASSERT_NEAR(concatenation3.y(),concatenation4.y(), 1e-3);
   ASSERT_NEAR(concatenation3.z(),concatenation4.z(), 1e-3);
+}
+
+// Testing vector() and constructor from vector
+TYPED_TEST (UnitQuaternionsSingleTest, testQuaternionSingleVectorAndVectorConstructor) {
+  typedef typename TestFixture::UnitQuaternion UnitQuaternion;
+  typedef typename TestFixture::UnitQuaternionScalar UnitQuaternionScalar;
+  typedef Eigen::Matrix<UnitQuaternionScalar, 4, 1> Vector4;
+  UnitQuaternion quat = this->quat1;
+
+  // vector()
+  Vector4 vector = quat.vector();
+  ASSERT_NEAR(quat.w(),vector(0), 1e-3);
+  ASSERT_NEAR(quat.x(),vector(1), 1e-3);
+  ASSERT_NEAR(quat.y(),vector(2), 1e-3);
+  ASSERT_NEAR(quat.z(),vector(3), 1e-3);
+
+  // constructor from vector
+  UnitQuaternion quatFromVector(vector);
+  ASSERT_NEAR(quat.w(),quatFromVector.w(), 1e-3);
+  ASSERT_NEAR(quat.x(),quatFromVector.x(), 1e-3);
+  ASSERT_NEAR(quat.y(),quatFromVector.y(), 1e-3);
+  ASSERT_NEAR(quat.z(),quatFromVector.z(), 1e-3);
 }
 
 
