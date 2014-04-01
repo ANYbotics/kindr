41a9d150c8e306af8d3fa29c050ce298a77d0597
Christian Gehring <gehrinch@ethz.ch>
2014-04-01 14:33:58 +0200

    Added headers for ros_tf

---
+++ include/kindr/thirdparty/ros_tf/RosTfEigen.hpp
@@ -0,0 +1,14 @@
+/*
+ * RosTfEigen.hpp
+ *
+ *  Created on: Apr 1, 2014
+ *      Author: gech
+ */
+
+#ifndef KINDR_ROSTFEIGEN_HPP_
+#define KINDR_ROSTFEIGEN_HPP_
+
+#include "kindr/thirdparty/ros_tf/RosTfRotationEigen.hpp"
+
+
+#endif /* KINDR_ROSTFEIGEN_HPP_ */
---
+++ include/kindr/thirdparty/ros_tf/RosTfRotationEigen.hpp
@@ -0,0 +1,21 @@
+/*
+ * RosTfRotationEigen.hpp
+ *
+ *  Created on: Apr 1, 2014
+ *      Author: gech
+ */
+
+#ifndef KINDR_ROSTFROTATIONEIGEN_HPP_
+#define KINDR_ROSTFROTATIONEIGEN_HPP_
+
+namespace kindr {
+namespace rotations {
+namespace eigen_impl {
+
+} // namespace eigen_impl
+} // namespace rotations
+} // namespace kindr
+
+
+
+#endif /* KINDR_ROSTFROTATIONEIGEN_HPP_ */
