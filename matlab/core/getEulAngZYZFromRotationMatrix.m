eaa1fd3102d541ccfc9273affe65a37f1c847e24
dbellicoso <bellicosodario@gmail.com>
2016-11-09 17:02:27 +0100

    Fixed Euler ZYZ mapping.

--- matlab/core/getEulAngZYZFromRotationMatrix.m
+++ matlab/core/getEulAngZYZFromRotationMatrix.m
@@ -5,7 +5,7 @@ function ph = getEulAngZYZFromRotationMatrix(C)
 % Author(s): Dario Bellicoso
 
 z1 = atan2(C(2,3),C(1,3));
-x = atan2(sqrt(C(1,3)^2+C(2,3)^2), C(3,3));
+y = atan2(sqrt(C(1,3)^2+C(2,3)^2), C(3,3));
 z2 = atan2(C(3,2),-C(3,1));
 
-ph = [z1 x z2]';
+ph = [z1 y z2]';
